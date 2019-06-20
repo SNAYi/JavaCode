bdb368efb7a5bbdbc5b2aa8855facafe7f77f65b
yourname <1141176009@qq.com>
2019-06-20 23:47:45 +0800

    In the encoding of a set of Numbers, if any two adjacent codes have only one binary number different from each other, the encoding is called Gray Code.
    Write a function that USES a recursive method to generate n-bit gray codes.

---
+++ GetGray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class GetGray {
+    public static String[] getGray(int n) {
+        //1.声明要返回的格雷码数组
+        String[] resStrs = null;
+        //2.递归出口
+        if(n == 1){
+            resStrs = new String[]{"0","1"};
+        }else{
+            //3.通过递归拿到n-1的格雷码
+            String[] strs = getGray(n-1);
+            //4.n的格雷码位数为n-1的2倍
+            resStrs = new String[2*strs.length];
+            //5.每一次循环从两端处理格雷码
+            for(int i=0; i<strs.length; i++){
+                resStrs[i] = "0"+strs[i];
+                resStrs[resStrs.length-1-i] = "1"+strs[i];
+            }
+        }
+        return resStrs;
+    }
+
+    public static void main(String[] args) {
+        String[] str = getGray(4);
+        for (String i: str) {
+            System.out.print(i + " ");
+        }
+    }
+}
